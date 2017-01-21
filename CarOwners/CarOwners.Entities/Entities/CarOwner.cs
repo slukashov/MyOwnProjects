19ae3d2eded836c8440354cb4c74de6cfcb36c04
serhii.lukashov <serhii.lukashov@nure.ua>
2017-01-21 14:58:33 +0200

    inplemented init. project
    add ef6, ninject, project with repos, entites

---
+++ CarOwners/CarOwners.Entities/Entities/CarOwner.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CarOwners.Entities.Entities
+{
+    public class CarOwner
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public float DrivingExperience { get; set; }
+
+        public virtual ICollection<Car> Cars { get; set; }
+    }
+}
---
+++ CarOwners/CarOwners.Repositories/Contexts/DatabaseContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity;
+using CarOwners.Entities.Entities;
+
+namespace CarOwners.Repositories.Contexts
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext() : base("CarOwnersContext")
+        {
+        }
+
+        public IDbSet<CarOwner> CarOwners { get; set; }
+        public IDbSet<Car> Car { get; set; }
+    }
+}
